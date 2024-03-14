bedf530c95948694a55ad823c36360e88c7e5511
Ajeet Kumar <ajeetkumar555368@gmail.com>
2024-03-15 01:56:32 +0530

    login signup form completed

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 
 //designing of component
@@ -7,27 +7,41 @@ import './LoginSignup.css'
 // import password_icon from '../Assets'
 
 const LoginSignup = () => {
+
+    const [action, setAction]= useState("Sign Up");
+
+
+
+
+
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">SignUp</div> 
+            <div className="text">{action}</div> 
             <div className="underline"></div>       
         </div>
         <div className="inputs">
+            {/* here we will return input div with text if action is Sign Up else return an empty div */}
+            {action==="Login"?<div></div>:
             <div className="input">
-                <img src={user_icon} alt="" /> 
-                <input type="text" /></div>
+                {/* <img src={user_icon} alt="" />  */}
+                <input type="text" placeholder='Enter your name' /></div>
+            }
             <div className="input">
-                <img src={email_icon} alt="" />
-                 <input type="email" /></div>
+                {/* <img src={email_icon} alt="" /> */}
+                 <input type="email" placeholder='Enter your email' /></div>
             <div className="input">
-                <img src={password_icon} alt=""
-                 /> <input type="password" /></div>
+                {/* <img src={password_icon} alt="" */}
+                 <input type="password" placeholder='Create new password'/></div>
         </div>
+        {/* Goal: Hide forgot password from signup form using ternary operator'?' */}
+        {action==="Sign Up"?<div></div>:
         <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+        }
         <div className="submit-container">
-          <div className="submit">Sign Up</div>
-          <div className="submit">Login</div>  
+            {/* dynamic classname using ternary operator */}
+          <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+          <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}} >Login</div>  
         </div>
     </div>
   )
